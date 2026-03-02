830699d881a85567375ade7566596f03171b6ba2
LukovDev <ruslanfelitsan@gmail.com>
2026-03-02 23:26:25 +0300

    commit 02 march 2026 23:26

--- src/cgdf/graphics/font.c
+++ src/cgdf/graphics/font.c
@@ -4,5 +4,30 @@
 
 
 // Подключаем:
-#include "../core/libs.h"
+#include <cgdf/core/std.h>
+#include <cgdf/core/libs.h>
+#include <cgdf/core/mm.h>
 #include "font.h"
+
+
+// -------- Вспомогательные функции: --------
+
+
+// -------- API шрифта: --------
+
+
+// Создать растровый шрифт:
+FontPixmap* FontPixmap_create() {
+    FontPixmap *font = (FontPixmap*)mm_alloc(sizeof(FontPixmap));
+
+    // Заполняем поля:
+    return font;
+}
+
+// Уничтожить растровый шрифт:
+void FontPixmap_destroy(FontPixmap **font) {
+    if (!font || !*font) return;
+
+    mm_free(*font);
+    *font = NULL;
+}
--- src/cgdf/graphics/font.h
+++ src/cgdf/graphics/font.h
@@ -3,3 +3,28 @@
 //
 
 #pragma once
+
+
+// Подключаем:
+#include <cgdf/core/std.h>
+#include <cgdf/core/libs.h>
+
+
+// Объявление структур:
+typedef struct FontPixmap FontPixmap;  // Растровый шрифт.
+
+
+// Растровый шрифт:
+struct FontPixmap {
+    //
+};
+
+
+// -------- API шрифта: --------
+
+
+// Создать растровый шрифт:
+FontPixmap* FontPixmap_create();
+
+// Уничтожить растровый шрифт:
+void FontPixmap_destroy(FontPixmap **font);
